71949bf5a45dad88145b572767fe01406ec7ce64
Kamil Adam <kamil.adam.zabinski@gmail.com>
2024-04-04 14:27:02 +0200

    Add generated code

---
+++ src/main/kotlin/pl/writeonly/omnibus/OmnibusApplication.kt
@@ -0,0 +1,11 @@
+package pl.writeonly.omnibus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OmnibusApplication
+
+fun main(args: Array<String>) {
+	runApplication<OmnibusApplication>(*args)
+}
---
+++ src/test/kotlin/pl/writeonly/omnibus/OmnibusApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.writeonly.omnibus
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OmnibusApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
