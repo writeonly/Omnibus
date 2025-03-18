b748dcad7dad20b0d0e15283e8964416f401296f
Kamil Adam <kamil.adam.zabinski@gmail.com>
2025-03-18 09:48:16 +0100

    Add rules

--- src/main/kotlin/pl/writeonly/omnibus/named/systems/polonez/Polonez.kt
+++ src/main/kotlin/pl/writeonly/omnibus/named/systems/polonez/Polonez.kt
@@ -1,6 +1,7 @@
 package pl.writeonly.omnibus.named.systems.polonez
 
 import io.vavr.Function1
+import io.vavr.PartialFunction
 import jakarta.inject.Named
 import pl.writeonly.omnibus.rule.Rule
 import pl.writeonly.omnibus.named.system.Bid
@@ -9,9 +10,9 @@ import pl.writeonly.omnibus.named.system.Context
 import io.vavr.collection.List as VavrList
 
 @Named
-class Polonez(val ruleList: List<Rule<Context, Bid>>): Function1<Context, Bid> {
+class Polonez(private val ruleList: List<Rule<Context, Bid>>): Function1<Context, Bid> {
     override fun apply(context: Context): Bid {
-        val rules: VavrList<Rule<Context, Bid>> = VavrList.ofAll(ruleList)
-        return rules.find { it.isDefinedAt(context) }.map { it.apply(context) }.getOrElse(Bid.Pass)
+        val rules: VavrList<PartialFunction<Context, Bid>> = VavrList.ofAll(ruleList)
+        return Rule.find(rules, context).getOrElse(Bid.Pass)
     }
 }
--- src/main/kotlin/pl/writeonly/omnibus/rule/Rule.kt
+++ src/main/kotlin/pl/writeonly/omnibus/rule/Rule.kt
@@ -1,13 +1,18 @@
 package pl.writeonly.omnibus.rule
 
-
 import io.vavr.PartialFunction
+
+import io.vavr.collection.List
+import io.vavr.control.Option
 
 interface Rule<A, B>: PartialFunction<A, B> {
 
     companion object {
-        fun <A, B> apply(a: A, fs: List<PartialFunction<A, B>>): B {
-            return fs.first { it.isDefinedAt(a) }.apply(a)
+        fun <A, B> find(rules: List<PartialFunction<A, B>>, context: A): Option<B> {
+            return rules.find { it.isDefinedAt(context) }.map { it.apply(context) }
+        }
+        fun <A, B> filter(rules: List<PartialFunction<A, B>>, context: A): List<B> {
+            return rules.filter { it.isDefinedAt(context) }.map { it.apply(context) }
         }
     }
 }
