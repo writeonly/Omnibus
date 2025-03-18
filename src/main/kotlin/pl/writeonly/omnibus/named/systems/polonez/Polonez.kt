bfcf1445cd466b78694e9093ab6575e8750d771d
Kamil Adam <kamil.adam.zabinski@gmail.com>
2025-03-18 09:51:35 +0100

    Add rules

--- src/main/kotlin/pl/writeonly/omnibus/named/systems/polonez/Polonez.kt
+++ src/main/kotlin/pl/writeonly/omnibus/named/systems/polonez/Polonez.kt
@@ -7,12 +7,14 @@ import pl.writeonly.omnibus.rule.Rule
 import pl.writeonly.omnibus.named.system.Bid
 import pl.writeonly.omnibus.named.system.Context
 
-import io.vavr.collection.List as VavrList
+import java.util.List as LegacyList
+
+import io.vavr.collection.List
 
 @Named
-class Polonez(private val ruleList: List<Rule<Context, Bid>>): Function1<Context, Bid> {
+class Polonez(private val ruleList: LegacyList<Rule<Context, Bid>>): Function1<Context, Bid> {
     override fun apply(context: Context): Bid {
-        val rules: VavrList<PartialFunction<Context, Bid>> = VavrList.ofAll(ruleList)
+        val rules: List<PartialFunction<Context, Bid>> = List.ofAll(ruleList)
         return Rule.find(rules, context).getOrElse(Bid.Pass)
     }
 }
