48789bc90c46f6822069d8ba30c58a4505a9de3b
Kamil Adam <kamil.adam.zabinski@gmail.com>
2025-03-19 18:12:15 +0100

    Add Osika

--- src/main/kotlin/pl/writeonly/omnibus/named/system/Osika.kt
+++ src/main/kotlin/pl/writeonly/omnibus/named/system/Osika.kt
@@ -3,6 +3,12 @@ package pl.writeonly.omnibus.named.system
 import io.vavr.collection.List
 
 object Osika {
+    fun countQuarters(hand: Hand): UInt = run {
+        val quarters = countHonorQuarters(hand)
+        val lengthBonus = hand.suits().values().map { lengthToQuarters(it.size().toUInt()) }.sumOf { it }
+        quarters + lengthBonus
+    }
+
     fun countHonorQuarters(hand: Hand): UInt = run {
         val quarters: UInt = hand.cards.sumOf { rankToQuarters(it.rank) }
         val seqBonus = hand.suits().values().map { calculateSeqBonus(it) }.sumOf { it }
@@ -17,13 +23,21 @@ object Osika {
         else -> 0u
     }
 
-    @Suppress("MagicNumber")
     private fun calculateSeqBonus(cards: List<Card>): UInt = run {
         val honorCount = cards.count { it.rank in listOf(Rank.ACE, Rank.KING, Rank.QUEEN, Rank.JACK) }
-        when (honorCount) {
-            2 -> 2u
-            3 -> 3u
+        when (honorCount.toUInt()) {
+            2u -> 2u
+            3u -> 3u
             else -> 0u
         }
+    }
+
+    private fun lengthToQuarters(l: UInt): UInt = when (l) {
+        4u -> 2u
+        5u -> 6u
+        6u -> 11u
+        7u -> 17u
+        8u -> 22u
+        else -> if (8u < l) lengthToQuarters(8u) else 0u
     }
 }
