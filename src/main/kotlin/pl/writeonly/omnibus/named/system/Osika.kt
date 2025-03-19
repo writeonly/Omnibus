7fee27d2358a5a647cb3ddafe511a1d9ac7282ca
Kamil Adam <kamil.adam.zabinski@gmail.com>
2025-03-19 17:03:48 +0100

    Add Osika

--- src/main/kotlin/pl/writeonly/omnibus/named/system/Osika.kt
+++ src/main/kotlin/pl/writeonly/omnibus/named/system/Osika.kt
@@ -5,11 +5,11 @@ import io.vavr.collection.List
 object Osika {
     fun countHonorQuarters(hand: Hand): UInt = run {
         val quarters: UInt = hand.cards.sumOf { rankToQuarters(it.rank) }
-        val seqBonus = hand.suits().values().map{calculateSeqBonus(it)}.sumOf { it }
+        val seqBonus = hand.suits().values().map { calculateSeqBonus(it) }.sumOf { it }
         quarters + seqBonus
     }
 
-    private fun rankToQuarters(rank: Rank): UInt =  when (rank) {
+    private fun rankToQuarters(rank: Rank): UInt = when (rank) {
         Rank.ACE -> 4u
         Rank.KING -> 3u
         Rank.QUEEN -> 2u
@@ -17,6 +17,7 @@ object Osika {
         else -> 0u
     }
 
+    @Suppress("MagicNumber")
     private fun calculateSeqBonus(cards: List<Card>): UInt = run {
         val honorCount = cards.count { it.rank in listOf(Rank.ACE, Rank.KING, Rank.QUEEN, Rank.JACK) }
         when (honorCount) {
