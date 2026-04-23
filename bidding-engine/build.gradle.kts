c597a10030961ca0c93d100c5d0ec63891a294a9
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-04-23 20:55:57 +0200

    Rewrite mvn to gradle

--- bidding-engine/build.gradle.kts
+++ bidding-engine/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
