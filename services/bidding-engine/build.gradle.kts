2d09e0f1aaf5236fc19253c1a6d5eae2ec4bfeb0
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-04-26 22:39:19 +0200

    FIX docker

--- services/bidding-engine/build.gradle.kts
+++ services/bidding-engine/build.gradle.kts
@@ -26,6 +26,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
 
+    // Kafka
+    implementation("org.springframework.kafka:spring-kafka")
+
     // Observability
     implementation("io.micrometer:micrometer-registry-prometheus")
 
