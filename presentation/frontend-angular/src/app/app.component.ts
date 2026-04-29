012c8ee8ad9664045c959c3fb90e4d7378cddd6f
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-04-29 10:20:04 +0200

    Add themas

--- presentation/frontend-angular/src/app/app.component.ts
+++ presentation/frontend-angular/src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RestBiddingComponent } from '@features/bidding/rest-bidding/rest-bidding.component';
 import { NextBidComponent } from '@features/bidding/next-bid/next-bid.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
+import { ThemeService } from './core/theme/theme.service';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
     selector: 'app-root',
@@ -10,8 +12,11 @@ import { MatCardModule } from '@angular/material/card';
       NextBidComponent,
       RestBiddingComponent,
       MatToolbarModule,
-      MatCardModule
+      MatCardModule,
+      MatSlideToggleModule
     ],
     templateUrl: './app.component.html'
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly theme = inject(ThemeService);
+}
