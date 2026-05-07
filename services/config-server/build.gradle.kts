356be356fc55ac3197f52413226f68e402c79864
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-05-07 20:20:42 +0200

    Add config-service

---
+++ services/config-server/build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.3.0"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+extra["springCloudVersion"] = "2023.0.3"
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // CONFIG SERVER
+    implementation("org.springframework.cloud:spring-cloud-config-server")
+
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
