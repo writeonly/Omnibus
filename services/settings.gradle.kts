f67b45c065ce0a2d7e940ed62ade337cadf63dfb
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-05-12 16:15:56 +0200

    Fix services

--- services/settings.gradle.kts
+++ services/settings.gradle.kts
@@ -11,13 +11,13 @@ include(
 
 )
 
-project(":config-server").projectDir = file("config-server")
-project(":api-gateway").projectDir = file("api-gateway")
-project(":bidding-engine").projectDir = file("bidding-engine")
-project(":workflow-orchestrator").projectDir = file("workflow-orchestrator")
-project(":event-archive").projectDir = file("event-archive")
-project(":outbox-relay-service").projectDir = file("outbox-relay-service")
-project(":cassandra-projection-service").projectDir = file("cassandra-projection-service")
+project(":config-server").projectDir = file("core/config-server")
+project(":api-gateway").projectDir = file("core/api-gateway")
+project(":bidding-engine").projectDir = file("core/bidding-engine")
+project(":workflow-orchestrator").projectDir = file("core/workflow-orchestrator")
+project(":event-archive").projectDir = file("legacy/event-archive")
+project(":outbox-relay-service").projectDir = file("legacy/outbox-relay-service")
+project(":cassandra-projection-service").projectDir = file("legacy/cassandra-projection-service")
 
 pluginManagement {
     repositories {
