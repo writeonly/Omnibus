53c5cab1b6bccae56cf033dbba32f88221fe9e24
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-05-12 17:59:53 +0200

    Work on gradle

--- services/core/bidding-engine/build.gradle.kts
+++ services/core/bidding-engine/build.gradle.kts
@@ -1,12 +1,13 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    id("org.springframework.boot")
-    id("io.spring.dependency-management")
-    id("com.google.protobuf") version "0.9.4"
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.deps)
 
-    kotlin("jvm")
-    kotlin("plugin.spring")
+    alias(libs.plugins.protobuf)
+
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.kotlin.spring)
 }
 
 java {
@@ -19,52 +20,50 @@ repositories {
     mavenCentral()
 }
 
-val droolsVersion = "8.44.0.Final"
-val springCloudVersion = "2023.0.5"
-val grpcVersion = "1.66.0"
-val protobufVersion = "3.25.5"
-
 dependencyManagement {
     imports {
-        mavenBom("org.springframework.cloud:spring-cloud-dependencies:$springCloudVersion")
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${libs.versions.springCloud.get()}")
     }
 }
 
 dependencies {
-    // Spring
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.cloud:spring-cloud-starter-function-web")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-jdbc")
-    implementation("net.devh:grpc-server-spring-boot-starter:3.1.0.RELEASE")
-    implementation("io.grpc:grpc-protobuf:$grpcVersion")
-    implementation("io.grpc:grpc-stub:$grpcVersion")
-    implementation("com.google.protobuf:protobuf-java-util:$protobufVersion")
-    compileOnly("javax.annotation:javax.annotation-api:1.3.2")
 
-    // Kafka
-    implementation("org.springframework.kafka:spring-kafka")
+    // ---------------- Spring ----------------
+    implementation(libs.spring.boot.starter.web)
+    implementation(libs.spring.cloud.starter.function.web)
+    implementation(libs.spring.boot.starter.validation)
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.boot.starter.jdbc)
 
-    // Observability
-    implementation("io.micrometer:micrometer-registry-prometheus")
+    // ---------------- gRPC ----------------
+    implementation(libs.grpc.spring.boot.starter)
+    implementation(libs.grpc.protobuf)
+    implementation(libs.grpc.stub)
+    implementation(libs.protobuf.java.util)
+    compileOnly(libs.javax.annotation.api)
 
-    // OpenAPI
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    // ---------------- Kafka ----------------
+    implementation(libs.spring.kafka)
 
-    // Kotlin
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // ---------------- Observability ----------------
+    implementation(libs.micrometer.registry.prometheus)
 
-    // DROOLS / KIE
-    implementation("org.kie:kie-api:$droolsVersion")
-    implementation("org.kie:kie-internal:$droolsVersion")
-    implementation("org.drools:drools-engine:$droolsVersion")
-    implementation("org.drools:drools-mvel:$droolsVersion")
-    implementation("org.drools:drools-decisiontables:$droolsVersion")
+    // ---------------- OpenAPI ----------------
+    implementation(libs.springdoc.openapi)
 
-    // Test
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // ---------------- Kotlin ----------------
+    implementation(libs.jackson.kotlin)
+    implementation(libs.kotlin.reflect)
+
+    // ---------------- DROOLS / KIE ----------------
+    implementation(libs.kie.api)
+    implementation(libs.kie.internal)
+    implementation(libs.drools.engine)
+    implementation(libs.drools.mvel)
+    implementation(libs.drools.decisiontables)
+
+    // ---------------- Test ----------------
+    testImplementation(libs.spring.boot.starter.test)
 }
 
 tasks.withType<Test>().configureEach {
@@ -79,12 +78,12 @@ kotlin {
 
 protobuf {
     protoc {
-        artifact = "com.google.protobuf:protoc:$protobufVersion"
+        artifact = "com.google.protobuf:protoc:${libs.versions.protobuf.get()}"
     }
 
     plugins {
         create("grpc") {
-            artifact = "io.grpc:protoc-gen-grpc-java:$grpcVersion"
+            artifact = "io.grpc:protoc-gen-grpc-java:${libs.versions.grpc.get()}"
         }
     }
 
