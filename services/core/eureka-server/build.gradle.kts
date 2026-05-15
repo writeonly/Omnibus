4e5334277645ee1d48107f016baf528379944db0
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-05-15 09:09:13 +0200

    Add Eureka server

---
+++ services/core/eureka-server/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.deps)
+
+    kotlin("jvm")
+    kotlin("plugin.spring")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom(
+            "org.springframework.cloud:spring-cloud-dependencies:${libs.versions.springCloud.get()}"
+        )
+    }
+}
+
+dependencies {
+    // Kotlin
+    implementation(libs.jackson.kotlin)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.kotlin.logging)
+
+    // Spring
+    implementation(libs.spring.boot.starter.actuator)
+    implementation(libs.spring.cloud.starter.config)
+    implementation(libs.spring.cloud.starter.netflix.eureka.server)
+
+    testImplementation(libs.spring.boot.starter.test)
+}
