d7aeaff91fb2f64f3064ad82e68dcd2c5fc04c22
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-05-25 22:22:20 +0200

    Add endpoints to gateway

--- api/server/api-gateway/src/main/kotlin/pl/writeonly/omnibus/gateway/controller/workflow/RestBiddingController.kt
+++ api/server/api-gateway/src/main/kotlin/pl/writeonly/omnibus/gateway/controller/workflow/RestBiddingController.kt
@@ -5,19 +5,22 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import pl.writeonly.omnibus.gateway.controller.grpcMono
 import pl.writeonly.omnibus.grpc.workflow.restbidding.v1.RestBiddingRequest
 import pl.writeonly.omnibus.grpc.workflow.restbidding.v1.RestBiddingResponse
 import pl.writeonly.omnibus.grpc.workflow.restbidding.v1.RestBiddingServiceGrpc
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/api/workflow/rest-bidding")
 class RestBiddingController(
     @GrpcClient("restBiddingService")
-    private val restBiddingService: RestBiddingServiceGrpc.RestBiddingServiceBlockingStub
+    private val stub: RestBiddingServiceGrpc.RestBiddingServiceStub
 ) {
     @PostMapping
-    fun register(@RequestBody body: RestBiddingRequest): RestBiddingResponse {
-        return restBiddingService.restBiddingBid(body)
+    fun register(@RequestBody body: RestBiddingRequest): Mono<RestBiddingResponse> {
+        return grpcMono { observer ->
+            stub.restBiddingBid(body, observer)
+        }
     }
-
 }
