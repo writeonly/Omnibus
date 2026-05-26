9e0d985d8dc05f90e6b0b8f8d1271ebbf63feaf0
Kamil Adam <kamil.adam.zabinski@gmail.com>
2026-05-26 15:25:25 +0200

    Change config for frontend

--- ui/frontend-react-vite/vite.config.ts
+++ ui/frontend-react-vite/vite.config.ts
@@ -3,13 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        changeOrigin: true,
-        secure: false
-      }
+server: {
+  proxy: {
+    '/api': {
+      target: 'http://localhost:8080',
+      changeOrigin: true,
+      secure: false
+    },
+
+    '/auth': {
+      target: 'http://localhost:9000',
+      changeOrigin: true,
+      secure: false
     }
   }
+}
 });
